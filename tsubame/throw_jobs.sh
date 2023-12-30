90433cfdf23fb3ac57c5d5faeb7ec4193bafe50f
Y.Hisaki <yhisaki31@gmail.com>
2023-12-30 20:23:28 +0900

    update tsubame

--- tsubame/throw_jobs.sh
+++ tsubame/throw_jobs.sh
@@ -3,15 +3,15 @@
 #$ -j y
 #$ -o out/
 #$ -l s_gpu=1
-#$ -l h_rt=02:00:00
+#$ -l h_rt=08:00:00
 #$ -t 1:10
 
 module load python/3.10.2 cuda/12.1.0 cudnn/8.8.1
 
 ENV_ID=Walker2d-v4
-SEED=$SGE_TASK_ID
 
 poetry run python3 experiments/main.py \
   env="$ENV_ID" \
   project=average-reward-drl \
-  seed="$SEED"
+  group="$ENV_ID-$JOB_ID" \
+  seed="$SGE_TASK_ID"
